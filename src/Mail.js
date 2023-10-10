ce78e23b68ad25dd213eb3bb8ba0c95c537f54c4
Gafdog <102024303+Gafdog@users.noreply.github.com>
2023-10-10 10:30:29 +1000

    Update Mail.js
    removing commented code

--- src/Mail.js
+++ src/Mail.js
@@ -20,52 +20,7 @@ function Mail() {
 .catch(err=>{
   console.log("Error:"+err)
 })
-    /*const mailchimpData = {
-      members: [
-        {
-          email_address: email,
-          status: 'subscribed',
-        },
-      ],
-    };
-
-    const mailchimpUrl = 'https://us21.api.mailchimp.com/3.0/lists/fb16edc462'; 
-    const mailchimpOptions = {
-      method: 'POST',
-      auth: 'azi:e1446934d028ad365be065463db76e4f-us21',
-    };
-
-    try {
-      const mailchimpResponse = await fetch(mailchimpUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(mailchimpData),
-      });
-
-      
-      if (mailchimpResponse.status === 200) {
-        
-        const sendGridResponse = await fetch('/send-welcome-email', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email }),
-        });
-
-        if (sendGridResponse.status === 200) {
-          console.log('Welcome email sent successfully.');
-        } else {
-          console.error('Failed to send welcome email.');
-        }
-      } else {
-        console.error('Failed to subscribe to Mailchimp.');
-      }
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }*/
+    
   };
 
   return (
